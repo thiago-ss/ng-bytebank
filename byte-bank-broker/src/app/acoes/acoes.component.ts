f4055e6da68f5fd22c9539871e539e3ba604e716
Thiago <thiago2508ss@gmail.com>
2022-07-21 15:54:02 -0300

    initial commit

---
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { AcoesService } from './acoes.service';
+import { merge } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs/operators';
+
+const ESPERA_DIGITACAO = 300;
+
+@Component({
+  selector: 'app-acoes',
+  templateUrl: './acoes.component.html',
+  styleUrls: ['./acoes.component.css'],
+})
+export class AcoesComponent{
+  acoesInput = new FormControl();
+  todasAcoes$ = this.acoesService.getAcoes();
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
+    filter(
+      valorDigitado => valorDigitado.length >= 3 || !valorDigitado.length
+    ),
+    distinctUntilChanged(),
+    switchMap(valorDigitado => this.acoesService.getAcoes(valorDigitado))
+  );
+
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
+
+  constructor(private acoesService: AcoesService) {}
+}
